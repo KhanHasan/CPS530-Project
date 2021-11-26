19e4e36ce1f632b74e1a056a15fffe1b2ba21c88
Hasan Khan <khan.hasan@hotmail.ca>
2021-11-26 10:36:39 -0800

    Format the NavBar

--- 530frontend/src/Navbar.js
+++ 530frontend/src/Navbar.js
@@ -2,22 +2,42 @@ import React from "react";
 // import * as ReactBootStrap from "react-bootstrap";
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  color: #D6D5CB;
+  text-decoration: none;
+  margin: 0.5rem;
+  font-size: 1.3em;
+  font-family: 'Varela Round', sans-serif;
+  position: relative;
+  text-align: right;
+`;
+
+const StyledTitle = styled(Link)`
+  color: White;
+  text-decoration: none;
+  margin: 0.5rem;
+  font-size: 1.7em;
+  position: relative;
+  text-align: right;
+`;
 
 function Navbar1() {
 return(
      <div className="NavBar1">
-<Navbar bg="light" expand="lg">
+<Navbar bg="primary" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">Hasans Page</Navbar.Brand>
+    <StyledTitle to="/">Hasans Page</StyledTitle>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Link to="./Page1">Page 1</Link>
-        <Link to="./Page2">Page 2</Link>
-        <Link to="./Page3">Page 3</Link>
-        <Link to="./Page4">Page 4</Link>
-        <Link to="./Page5">Page 5</Link>
-        <Link to="./Page6">Page 6</Link>
+        <StyledLink to="./Page1">Page 1</StyledLink>
+        <StyledLink to="./Page2">Page 2</StyledLink>
+        <StyledLink to="./Page3">Page 3</StyledLink>
+        <StyledLink to="./Page4">Page 4</StyledLink>
+        <StyledLink to="./Page5">Page 5</StyledLink>
+        <StyledLink to="./Page6">Page 6</StyledLink>
       </Nav>
     </Navbar.Collapse>
   </Container>
